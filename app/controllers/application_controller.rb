79640fb57da71b83a35628c01f0841da05c68557
Ed Jefferson <e.jefferson@gmail.com>
2021-02-07 10:07:04 +0000

    retrieval

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 require 'json'
 class ApplicationController < ActionController::API
-  def create
-    if params["record"] && params["project_id"]
-      Record.create(project_id: params["project_id"],json_field: params["record"].to_json)
-    end
-    render :json => ["egg"]
-
-  end
 end
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,19 @@
 class RecordsController < ApplicationController
+  def create
+    if params["record"] && params["project_id"]
+      Record.create(project_id: params["project_id"],json_field: params["record"].to_json)
+    end
+    render :json => ["egg"]
+
+  end
+
+  def retrieve
+    if params["offset"]
+      records = Record.where("id < #{params["offset"]}").order(id: :desc).limit(100)
+
+    else
+      records = Record.order(id: :desc).limit(100)
+    end
+    render :json => records
+  end
 end
